3e5865a0dcb202d31abb16e1426d7197870c2641
Alexander Georgiev <wishiper@gmail.com>
2020-04-19 21:57:20 +0300

    spring boot initial project

---
+++ src/main/java/agprojects/pokerapp/PokerappApplication.java
@@ -0,0 +1,13 @@
+package agprojects.pokerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerappApplication.class, args);
+	}
+
+}
